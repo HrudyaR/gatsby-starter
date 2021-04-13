e65566d086ea5fa5ae056425f65f5ed4f19748e5
JINAL-1 <59589615+JINAL-1@users.noreply.github.com>
2021-04-13 10:49:35 -0700

    Sweet Demo

--- src/components/header.js
+++ src/components/header.js
@@ -20,13 +20,13 @@ const Header = ({ siteTitle }) => (
         </Link>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link to="/about" className="nav-link" aria-current="page">About</Link>
-                </li>
+                  <li className="nav-item">
+                    <Link to="/about" className="nav-link" aria-current="page">About</Link>
+                  </li>
 
-                <li className="nav-item">
-                  <Link to="/demo" className="nav-link" aria-current="page">Sweet</Link> 
-                </li>
+                  <li className="nav-item">
+                    <Link to="/demo" className="nav-link" aria-current="page">Sweet</Link> 
+                  </li>
             </ul>
         </div>
       </div>
